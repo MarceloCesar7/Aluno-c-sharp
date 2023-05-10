75ee5c94bf4c8c191e92103ca76a8d878ee51c2d
Marcelo Cesar <marcelocesar9812@gmail.com>
2023-05-10 15:54:42 -0300

    first commit

---
+++ Aluno/Program.cs
@@ -0,0 +1,21 @@
+﻿using Projeto;
+using System.Globalization;
+
+Aluno a = new Aluno();
+Console.WriteLine("Emtre com o nome do Aluno");
+a.Nome = Console.ReadLine();
+Console.WriteLine("Digite as 3 notas do aluno");
+a.Nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+a.Nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.WriteLine("Nota final: "+ a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+if (a.Aprovado())
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+    Console.WriteLine("FALTARAM: " + a.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + "pontos");
+}
